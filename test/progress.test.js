b36b3f9144886fc48b14d5126cf77844f81703f2
Alexander Eng <alexbird300@gmail.com>
2026-05-06 23:46:00 -0700

    old formula was messing with github

--- test/progress.test.js
+++ test/progress.test.js
@@ -1,10 +1,19 @@
 import test from "node:test";
 import assert from "node:assert/strict";
-import { calculateLevel } from "../src/utils/progress.js";
+import { calculateLevel, getXpRequiredForNextLevel } from "../src/utils/progress.js";
 
-test("calculateLevel increases every 100 xp", () => {
+test("getXpRequiredForNextLevel scales by current level", () => {
+  assert.equal(getXpRequiredForNextLevel(1), 100);
+  assert.equal(getXpRequiredForNextLevel(2), 150);
+  assert.equal(getXpRequiredForNextLevel(3), 200);
+});
+
+test("calculateLevel uses scaled xp thresholds", () => {
   assert.equal(calculateLevel(0), 1);
   assert.equal(calculateLevel(99), 1);
   assert.equal(calculateLevel(100), 2);
-  assert.equal(calculateLevel(240), 3);
+  assert.equal(calculateLevel(249), 2);
+  assert.equal(calculateLevel(250), 3);
+  assert.equal(calculateLevel(449), 3);
+  assert.equal(calculateLevel(450), 4);
 });
